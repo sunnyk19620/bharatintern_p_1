c5e3b9cb3f92105176c9baf03afc44eabc6563cd
Sunny kumar <162075981+sunnyk19620@users.noreply.github.com>
2024-08-10 01:20:07 +0530

    Add files via upload
    bharat intern project 1

---
+++ script.js
@@ -0,0 +1,17 @@
+document.getElementById('contact-form').addEventListener('submit', function(event) {
+    event.preventDefault();
+  
+    const name = document.getElementById('name').value;
+    const email = document.getElementById('email').value;
+    const message = document.getElementById('message').value;
+  
+    console.log('Form Submitted!');
+    console.log('Name:', name);
+    console.log('Email:', email);
+    console.log('Message:', message);
+  
+    alert('Thank you for your message!');
+  
+    document.getElementById('contact-form').reset();
+  });
+  
